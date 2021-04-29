d3920d526c482a9a400586003cc43332b35cee46
shibuiwilliam <shibuiyusuke@gmail.com>
2021-04-29 17:30:41 +0900

    update

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -8,18 +8,20 @@ using UnityEngine.XR.ARSubsystems;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject cube;
-    private ARRaycastManager aRRaycastManager;
+    private ARRaycastManager arRaycastManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     // Start is called before the first frame update
     void Start()
     {
-        aRRaycastManager = GetComponent<ARRaycastManager>();
+
+        arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -28,7 +30,7 @@ public class SpawnManager : MonoBehaviour
                 if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
                 {
                     Pose hitPose = hits[0].pose;
-                    Instantiate(redCube, hitPose.position, hitPose.rotation);
+                    Instantiate(cube, hitPose.position, hitPose.rotation);
                 }
             }
         }
