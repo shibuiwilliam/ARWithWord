e590823055f58913663bd2c424d31320689ac3db
shibuiwilliam <shibuiyusuke@gmail.com>
2021-05-01 16:54:10 +0900

    update

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+
+    private ARRaycastManager arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    
+                    Pose hitPose = hits[0].pose;
+                    Instantiate(text, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
