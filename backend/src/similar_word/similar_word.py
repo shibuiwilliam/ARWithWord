bb81986b41faa7a6e9cfd488d020452e7c3e9d45
shibuiwilliam <shibuiyusuke@gmail.com>
2021-05-05 17:49:47 +0900

    update

--- backend/src/similar_word/similar_word.py
+++ backend/src/similar_word/similar_word.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen
 import fasttext
 import fasttext.util
 from gensim.models.fasttext import load_facebook_model
+from google.cloud import storage
 
 from logging import getLogger
 from typing import List, Tuple, Dict
@@ -20,12 +21,21 @@ logger = getLogger(__name__)
 class SimilarWordPredictor(object):
     def __init__(
         self,
-        model_directory: str = "/opt/model/",
+        bucket_name: str,
+        model_directory: str = "/opt/",
         language: LANGUAGE_ENUM = LANGUAGE_ENUM.ENGLISH,
+        model_dimension: int = 100,
         threshold: float = 0.6,
     ):
+        self.bucket_name = bucket_name
+        self.client = storage.Client()
+        self.bucket = self.client.get_bucket(self.bucket_name)
+
         self.model_directory = model_directory
         self.language = language
+        self.model_dimension = model_dimension
+        if self.model_dimension not in [100, 300]:
+            raise ValueError("model dimension must be one of 100 or 300")
 
         self.file_path = self.download_model(if_exists="ignore")
         self.fasttext_predictor = load_facebook_model(self.file_path)
@@ -70,52 +80,31 @@ class SimilarWordPredictor(object):
 
         return predictions
 
-    def _download_file(
-        self,
-        url: str,
-        write_file_name: str,
-        chunk_size: int = 2 ** 13,
-    ):
-        response = urlopen(url)
-        downloaded = 0
-        download_file_name = f"{write_file_name}.part"
-        with open(download_file_name, "wb") as f:
-            while True:
-                chunk = response.read(chunk_size)
-                downloaded += len(chunk)
-                if not chunk:
-                    break
-                f.write(chunk)
-        os.rename(download_file_name, write_file_name)
-
     def download_model(
         self,
         if_exists: str = "strict",
     ):
         file_name = f"cc.{self.language.value}.300.bin"
-        gz_file_name = f"{file_name}.gz"
         file_path = os.path.join(self.model_directory, file_name)
-        gz_file_path = os.path.join(self.model_directory, gz_file_name)
+        logger.info(f"retrieve model {file_name}")
 
         if os.path.exists(file_path):
             if if_exists == "ignore":
+                logger.info(f"model {file_name} exists")
                 return file_path
 
-        url = f"https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/{gz_file_name}"
+        blob = self.bucket.blob(file_name)
+        blob.download_to_filename(file_path)
 
-        self._download_file(
-            url=url,
-            write_file_name=gz_file_path,
-        )
-        with gzip.open(gz_file_path, "rb") as f:
-            with open(file_path, "wb") as f_out:
-                shutil.copyfileobj(f, f_out)
+        logger.info(f"retrieved model {file_name}")
 
         return file_path
 
 
 similar_word_predictor = SimilarWordPredictor(
-    model_directory=os.getenv("MODEL_DIRECTORY", "/opt/model/"),
+    bucket_name=os.getenv("BUCKET_NAME", ""),
+    model_directory=os.getenv("MODEL_DIRECTORY", "/opt/"),
     language=LANGUAGE_ENUM[os.getenv("LANGUAGE", "ENGLISH").upper()],
+    model_dimension=int(os.getenv("MODEL_DIMENSION", 100)),
     threshold=float(os.getenv("THRESHOLD", 0.6)),
 )
